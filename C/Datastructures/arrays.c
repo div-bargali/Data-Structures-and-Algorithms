19080ba0ce61d4678e78af36fbc12d9d1072dce2
iamvs-2002 <iamvs2002@gmail.com>
2020-10-06 15:13:12 +0530

    Added Array implementation in C

---
+++ C/Datastructures/arrays.c
@@ -0,0 +1,21 @@
+//accessing the array elements
+
+#include <stdio.h>
+ 
+int main () {
+
+   int n[ 10 ]; /* n is an array of 10 integers */
+   int i,j;
+ 
+   /* initialize elements of array n to 0 */         
+   for ( i = 0; i < 10; i++ ) {
+      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   }
+   
+   /* output each array element's value */
+   for (j = 0; j < 10; j++ ) {
+      printf("Element[%d] = %d\n", j, n[j] );
+   }
+ 
+   return 0;
+}
