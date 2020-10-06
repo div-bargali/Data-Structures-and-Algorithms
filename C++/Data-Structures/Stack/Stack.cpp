a65a5d363b57081b481a4f2f2da34afad8eed9d8
vishwajeet7200 <43901984+vishwajeet7200@users.noreply.github.com>
2020-10-06 20:45:08 +0530

    Update Stack.cpp
    basic example with the help of STL

--- C++/Data-Structures/Stack/Stack.cpp
+++ C++/Data-Structures/Stack/Stack.cpp
@@ -1,45 +1,18 @@
-#include <bits/stdc++.h>   //we are using cp header
-using namespace std; 
-  
-void showstack(stack <int> s)  //function for stack
-{ 
-    while (!s.empty()) 
-    { 
-        cout << '\t' << s.top(); 
-        s.pop(); 
-    } 
-    cout << '\n'; 
-} 
-  
-int main () 
-{ 
-    stack <int> s;    // declaring stack
-    s.push(10);                   //pushing elements
-    s.push(30); 
-    s.push(20); 
-    s.push(5); 
-    s.push(1); 
-  
-    cout << "The stack is : "; 
-    showstack(s);                   //calling our stack function
-  
-    cout << "\ns.size() : " << s.size(); 
-    cout << "\ns.top() : " << s.top(); 
-  
-  
-    cout << "\ns.pop() : "; 
-    s.pop(); 
-    showstack(s); 
-  
-    return 0; 
-} 
+#include <iostream> 
+#include <stack> 
+using namespace std;
+int main() {
+	stack<int> st;
+	st.push(10);
+	st.push(20);
+	st.push(30);
+	st.push(40);
+	
+         st.pop();
+	st.pop();
 
-
-/*
-Output:
-The stack is :     1    5    20    30    10
-
-s.size() : 5
-s.top() : 1
-s.pop() :     5    20    30    10  
-*/
+	while (!st.empty()) {
+		cout << ' ' << st.top();
+		st.pop();
+	}
+}
