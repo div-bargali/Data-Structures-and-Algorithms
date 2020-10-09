eae507b186dbf6e73bbda410295debe315d7d3db
Kotecha Udit Hitendra <64374551+kirito-udit@users.noreply.github.com>
2020-10-09 17:29:02 +0530

    Circular Linked List
    This program contains functions for
    1) Creation of a node
    2) Addition after a give node
    3) Addition of a node at the end of the list
    4) Addition of a head node
    5) Traversal of the Circular Linked List

---
+++ C++/Data-Structures/LinkedList/Circular Linked List Operations.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+};
+
+struct Node *addToEmpty(struct Node *last, int data)
+{
+	// This function is only for empty list
+	if (last != NULL)
+	return last;
+
+	// Creating a node dynamically.
+	struct Node *temp = 
+		(struct Node*)malloc(sizeof(struct Node));
+
+	// Assigning the data.
+	temp -> data = data;
+	last = temp;
+
+	// Creating the link.
+	last -> next = last;
+
+	return last;
+}
+
+struct Node *addBegin(struct Node *last, int data)
+{
+	if (last == NULL)
+		return addToEmpty(last, data);
+
+	struct Node *temp = 
+			(struct Node *)malloc(sizeof(struct Node));
+
+	temp -> data = data;
+	temp -> next = last -> next;
+	last -> next = temp;
+
+	return last;
+}
+
+struct Node *addEnd(struct Node *last, int data)
+{
+	if (last == NULL)
+		return addToEmpty(last, data);
+	
+	struct Node *temp = 
+		(struct Node *)malloc(sizeof(struct Node));
+
+	temp -> data = data;
+	temp -> next = last -> next;
+	last -> next = temp;
+	last = temp;
+
+	return last;
+}
+
+struct Node *addAfter(struct Node *last, int data, int item)
+{
+	if (last == NULL)
+		return NULL;
+
+	struct Node *temp, *p;
+	p = last -> next;
+	do
+	{
+		if (p ->data == item)
+		{
+			temp = (struct Node *)malloc(sizeof(struct Node));
+			temp -> data = data;
+			temp -> next = p -> next;
+			p -> next = temp;
+
+			if (p == last)
+				last = temp;
+			return last;
+		}
+		p = p -> next;
+	} while(p != last -> next);
+
+	cout << item << " not present in the list." << endl;
+	return last;
+
+}
+
+void traverse(struct Node *last)
+{
+	struct Node *p;
+
+	// If list is empty, return.
+	if (last == NULL)
+	{
+		cout << "List is empty." << endl;
+		return;
+	}
+
+	// Pointing to first Node of the list.
+	p = last -> next;
+
+	// Traversing the list.
+	do
+	{
+		cout << p -> data << " ";
+		p = p -> next;
+
+	}
+	while(p != last->next);
+
+}
+
+// Driven Program
+int main()
+{
+	struct Node *last = NULL;
+
+	last = addToEmpty(last, 6);
+	last = addBegin(last, 4);
+	last = addBegin(last, 2);
+	last = addEnd(last, 8);
+	last = addEnd(last, 12);
+	last = addAfter(last, 10, 8);
+
+	traverse(last);
+
+	return 0;
+}
