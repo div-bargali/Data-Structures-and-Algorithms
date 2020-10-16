aa61bd667b45348446b0a564fd22ecce7786d46f
Abhishek Jain <55635206+fiddlytompkus@users.noreply.github.com>
2020-10-16 15:23:56 +0530

    Update arrays.c

--- C/Data-Structures/Arrays/arrays.c
+++ C/Data-Structures/Arrays/arrays.c
@@ -4,16 +4,20 @@
  
 int main () {
 
-   int n[ 10 ]; /* n is an array of 10 integers */
+   int capacity; //aray size user input
+   
+   printf("Enter the array Size");
+   scanf("%d",&capacity);
+   int n[ capacity ]; /* n is an array of 10 integers */
    int i,j;
  
    /* initialize elements of array n to 0 */         
-   for ( i = 0; i < 10; i++ ) {
+   for ( i = 0; i < capacity; i++ ) {
       n[ i ] = i + 100; /* set element at location i to i + 100 */
    }
    
    /* output each array element's value */
-   for (j = 0; j < 10; j++ ) {
+   for (j = 0; j < capacity; j++ ) {
       printf("Element[%d] = %d\n", j, n[j] );
    }
  
