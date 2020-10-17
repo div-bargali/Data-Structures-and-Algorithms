b589a144f77db408a2d1f83cb63396ebd9227250
Pedro Perez <pedropc@usp.br>
2020-10-17 03:24:32 -0300

    Adding linear search algorithm in C

--- C/Algorithms/Searching-Algorithms/linear_search.c
+++ C/Algorithms/Searching-Algorithms/linear_search.c
@@ -2,38 +2,41 @@
 #include<stdio.h>
 
 int *input_array(int);
-
-int recursive_linear_search(int, int*, int, int);
+int linear_search(int, int*, int);
+int input_integer();
 
 int main(int argc, char** argv){
-    int size_array;
-    scanf("%d", &size_array);
-
+    int size_array = input_integer();
     int *array = input_array(size_array);
-
-    int key;
-    scanf("%d", &key);
-
-    int found = 
-    free(array);    //Making sure to free the memory allocated to the array
+    int key = input_integer();
+    int found = linear_search(key, array, size_array);
     return 0;
 }
 
-//Allocates and inputs values to an array of size_array values. Returns the array.
-int *input_array(int size_array){
-    int *array = (int*)malloc(size_array * sizeof(int));    //Creating an array in heap memory
-    for(int i = 0; i < size_array; i++){
+//Returns an integer from stdin
+int input_integer(){
+    int integer;
+    scanf("%d", &integer);
+    return integer;
+}
+
+//Creates an array of size elements in the heap memory. Return the pointer to the array
+int *input_array(int size){
+    int *array = malloc(size * sizeof(int));
+    for(int i = 0; i < size; i++){
         scanf("%d", &array[i]);
     }
     return array;
 }
 
-//Looks for a key in the array of size_array items using linear search logic, it starts at the beggining of the array and goes all the way until it finds the key or reaches the end. 
-//Returns the position of the item in the array if found.If not found returns -1.
-int linear_search(int key, int *array, int size_array){
+//Looks for the key in the array of size elements. Returns the position of key in the array or -1 if key is not in the array
+int linear_search(int key, int* array, int size){
     int found = -1;
-    for(int i = 0; i < size_array; i++)
-        if(key == array[i])
+    for(int i = 0; i < size; i++){
+        if(key == array[i]){
             found = i;
+        }
+    }
+    free(array);    //Making sure to free the memory allocated to the array
     return found;
 }
