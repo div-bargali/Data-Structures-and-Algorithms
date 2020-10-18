ef0d18b004a19dbd02d3642d7a9d020034abbed0
Mohamed Khaled Yousef <mkhy88519@gmail.com>
2020-10-18 11:03:48 +0200

    Update Shell_Sort.cpp

--- C++/Algorithms/Sorting-Algorithms/ShellSort/Shell_Sort.cpp
+++ C++/Algorithms/Sorting-Algorithms/ShellSort/Shell_Sort.cpp
@@ -1,3 +1,10 @@
+//array size
+// 5
+//array before shell sorting
+// 3 5 8 4 2
+//array after shell sorting
+// 2 3 4 5 8
+
 #include <iostream>
 
 using namespace std;
@@ -6,16 +13,19 @@ int main()
 {
     int size;
     cin >>size;
-
+    
+    // Create an array with given number of elements
     int x[size];
 
+    // Take the array of numbers
     for (int n=0 ; n<size ;++n)
             cin>>x[n];
 
     int i=0,j=0,key;
 
     int increment=(size/2);
-
+    
+    // Shellsort implementation
     while (increment !=0)
     {
         i=increment;
