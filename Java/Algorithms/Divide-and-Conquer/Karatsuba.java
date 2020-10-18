cb97929cea4321d670714e13e73d20e73872e9b2
almas33 <54628502+almas33@users.noreply.github.com>
2020-10-18 21:32:29 +0530

    Add Karatsuba algorithm in java

--- Java/Algorithms/Divide-and-Conquer/Karatsuba.java
+++ Java/Algorithms/Divide-and-Conquer/Karatsuba.java
@@ -9,23 +9,23 @@
 
 public class Karatsuba
 {
-	public static int count(int num)                           					//count digits in a number
-	{
-        int cnt =0; 
-        while(num != 0)
-        {
-            num /= 10;
-            ++cnt;
-        }
-        return cnt;
-	}
+    public static int count(int num)                           					//count digits in a number
+    {
+       int cnt =0; 
+       while(num != 0)
+       {
+           num /= 10;
+           ++cnt;
+       }
+       return cnt;
+     }
 	
-    public static int ceil(int num)      										//to return ceil of a number
+    public static int ceil(int num)      							//to return ceil of a number
     {
         return (num%2==1)?(num/2)+1:num/2;
-	}
+    }
 	
-    public static int pow10(int num)      										//this function takes number and return 10^number
+    public static int pow10(int num)      							//this function takes number and return 10^number
     {
         if(num==0)
             return 1;
@@ -39,21 +39,20 @@ public class Karatsuba
         
         int n=Math.max(count(x),count(y));
         
-        int l=ceil(n/2);                                                       //take ceil of n/2 in case no. is odd
+        int l=ceil(n/2);                                                                        //take ceil of n/2 in case no. is odd
 
-        int xl=x/pow10(l);                                                     //left part of x
-        int xr=x%pow10(l);													   //right part of x
+        int xl=x/pow10(l);                                                                      //left part of x
+        int xr=x%pow10(l);									//right part of x
         
-        int yl=y/pow10(l);													   //left part of y
-		int yr=y%pow10(l);													   //right part of y
+        int yl=y/pow10(l);									//left part of y
+	int yr=y%pow10(l);									//right part of y
 		
-		int xlyl=karatsuba(xl,yl);
-		int xryr=karatsuba(xr,yr);
+	int xlyl=karatsuba(xl,yl);
+	int xryr=karatsuba(xr,yr);
 
-		//(xr+xl)(yr+yl) - xlyl - xryr =  xlyr + ylxr
+	//(xr+xl)(yr+yl) - xlyl - xryr =  xlyr + ylxr
 		
-        return xlyl*pow10(2*l) +   (karatsuba(xr+xl,yr+yl) - xlyl - xryr)*pow10(l)    + xryr;
-		
+        return xlyl*pow10(2*l) +   (karatsuba(xr+xl,yr+yl) - xlyl - xryr)*pow10(l)  + xryr;	
     }
     
 	public static void main(String[] args) {
