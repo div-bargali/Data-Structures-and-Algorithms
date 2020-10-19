a905d47c9f8bdc3e4df622433a510325c7794ef3
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-10-19 19:57:37 -0200

    Update Factorial.c

--- C/Algorithms/Mathematical/Factorial.c
+++ C/Algorithms/Mathematical/Factorial.c
@@ -1,19 +1,19 @@
 /**
- *					Factorial Algorithm
+ * Factorial Algorithm
  *
  * Mathematical explanation:
- *		The factorial function has the following format:
- *			factorial(n) = !n = n.!(n-1) = n.(n-1).(n-2). ... !0
- *		Where,
- *			factorial(0) = !0 = 1
- *			factorial(1) = !1 = 1.!0 = 1
- *			factorial(2) = !2 = 2.!1 = 2
- *			...
- *			factorial(n) = !n = n.!(n-1) = n.(n-1).(n-2). ... !0
+ *	The factorial function has the following format:
+ *		factorial(n) = !n = n.!(n-1) = n.(n-1).(n-2). ... !0
+ *	Where,
+ * 		factorial(0) = !0 = 1
+ * 		factorial(1) = !1 = 1.!0 = 1
+ *		factorial(2) = !2 = 2.!1 = 2
+ *		...
+ *		factorial(n) = !n = n.!(n-1) = n.(n-1).(n-2). ... !0
  *
  * Code explanation:
- * 		factorial(n) - calculates the factorial to the index `n`
- *		example()	 - example of how factorial function works
+ *	factorial(n) - calculates the factorial to the index `n`
+ * 	example()	 - example of how factorial function works
  */
 #include <stdio.h>
 
