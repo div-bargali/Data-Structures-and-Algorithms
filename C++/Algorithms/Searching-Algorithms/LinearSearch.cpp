6ab0f13ef40bb8f74152e88384faad7d353dd256
Shreekar Kolanu <43133388+Skillz619@users.noreply.github.com>
2020-10-22 14:00:31 +0530

    Update LinearSearch.cpp

--- C++/Algorithms/Searching-Algorithms/LinearSearch.cpp
+++ C++/Algorithms/Searching-Algorithms/LinearSearch.cpp
@@ -5,11 +5,11 @@
 #include <iostream> 
 using namespace std; 
 
-int search(int arr[], int n, int y) 
+int search(int arr[], int n, int x) 
 { 
 	int i; 
 	for (i = 0; i < n; i++) 
-		if (arr[i] == y) 
+		if (arr[i] == x) 
 			return i; 
 	return -1; 
 } 
@@ -17,9 +17,9 @@ int search(int arr[], int n, int y)
 int main(void) 
 { 
 	int arr[] = { 2, 3, 4, 10, 40 }; 
-	int y = 10; 
+	int x = 10; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
-	int result = search(arr, n, y); 
+	int result = search(arr, n, x); 
 (result == -1)? cout<<"Element is not present in array"
 				: cout<<"Element is present at index " <<result; 
 return 0; 
