6ebac033b42570af56eee1eeb4e1642e7cae383a
nishant chavan <nishantscg@gmail.com>
2020-10-22 19:35:53 +0530

    mergesort added in go

---
+++ Go/Algorithms/Sorting-Algorithms/MergeSort/MergeSort.go
@@ -0,0 +1,33 @@
+// Runs MergeSort algorithm on a slice single
+func MergeSort(slice []int) []int {
+
+	if len(slice) < 2 {
+		return slice
+	}
+	mid := (len(slice)) / 2
+	return Merge(MergeSort(slice[:mid]), MergeSort(slice[mid:]))
+}
+
+// Merges left and right slice into newly created slice
+func Merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
+}
