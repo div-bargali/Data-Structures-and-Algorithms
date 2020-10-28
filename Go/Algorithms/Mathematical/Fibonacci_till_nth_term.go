3ac841448848671c578cd9f3027649ae2b408ee8
brisingr24 <rawatadity234@gmail.com>
2020-10-28 19:22:34 +0530

    Add Fibonacci_till_nth_term

---
+++ Go/Algorithms/Mathematical/Fibonacci_till_nth_term.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+        n:=4
+	a:=0
+	b:=1
+	sum:=0
+	if n<2{
+	fmt.Print("Fibonacci sereies till 1st term is 0")
+	}else
+	if n<3{
+	fmt.Print("Fibonacci sereies till 2nd term is 0 1")
+	}else{
+	fmt.Print("Fibonacci sereies till ",n,"th term is ",a," ",b," " )
+	for i:=2;i<n;i++{
+	
+		sum=a+b
+		fmt.Print(sum," ")
+		a=b
+		b=sum
+		}
+		
+}
+}
