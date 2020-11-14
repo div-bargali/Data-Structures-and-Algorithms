6607a8a9bbc8ff22332da7bb2c420ce06e86df95
sanggeun <suki5038@naver.com>
2020-11-14 21:49:43 +0900

    Permutation_of_string Error fixing

--- C++/Algorithms/Backtracking/Permutation_of_string.cpp
+++ C++/Algorithms/Backtracking/Permutation_of_string.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Function to find all Permutations of a given string str[i..n-1]
 // containing all distinct characters and which u want to change
 
-void permutations(string str, int i, int n,int j, int k )
+void permutations(string str, int i, int n)
 {
     // base condition
     if (i == n - 1)
