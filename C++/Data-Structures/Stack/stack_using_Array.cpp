a47d640a4846de60f370dac8de5ffcb37ecbff19
sanggeun <suki5038@naver.com>
2020-12-03 03:28:54 +0900

    Add Commit Stack implementation using array

--- C++/Data-Structures/Stack/stack_using_Array.cpp
+++ C++/Data-Structures/Stack/stack_using_Array.cpp
@@ -2,69 +2,69 @@
 using namespace std;
 
 template<typename T>
-class StackUsingArray {
-	T *data;
-	int nextIndex;
-	int capacity;
+class StackUsingArray {                        //create class
+   T *data;
+   int nextIndex;
+   int capacity;
 public:
-	StackUsingArray() {
-		data = new T[capacity];
-		capacity = 4;
-		nextIndex = 0;
-	}
-	int size() {
-		return nextIndex;
-	}
-	bool isEmpty() {
-		return nextIndex == 0;
-	}
-	void push(T n) {
-		if (nextIndex == capacity) {
-			T *newData = new T[2 * capacity];
-			for (int i = 0; i < capacity; i++) {
-				newData[i] = data[i];
-			}
-			capacity = capacity * 2;
-			delete[] data;
-			data = newData;
+   StackUsingArray() {
+      data = new T[capacity];
+      capacity = 4;
+      nextIndex = 0;
+   }
+   int size() {                           //get size
+      return nextIndex;
+   }
+   bool isEmpty() {                        //get empty or not of stack
+      return nextIndex == 0;
+   }
+   void push(T n) {                        //insert data
+      if (nextIndex == capacity) {
+         T *newData = new T[2 * capacity];
+         for (int i = 0; i < capacity; i++) {
+            newData[i] = data[i];
+         }
+         capacity = capacity * 2;
+         delete[] data;
+         data = newData;
 
-		}
-		data[nextIndex] = n;
-		nextIndex++;
-	}
-	T pop() {
-		if (isEmpty() == true) {
-			cout << "Stack is empty" << endl;
-			return 0;
-		}
-		nextIndex--;
-		return data[nextIndex];
-	}
-	T top() {
-		return data[nextIndex - 1];
-	}
+      }
+      data[nextIndex] = n;
+      nextIndex++;
+   }
+   T pop() {                              //delete data
+      if (isEmpty() == true) {
+         cout << "Stack is empty" << endl;
+         return 0;
+      }
+      nextIndex--;
+      return data[nextIndex];
+   }
+   T top() {                              //return    
+      return data[nextIndex - 1];
+   }
 
 };
-int main() {
+int main() {                              //driver
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
 #endif
 
-	StackUsingArray<int> s;
-	s.push(101);
-	s.push(102);
-	s.push(103);
-	s.push(104);
-	s.push(105);
+   StackUsingArray<int> s;
+   s.push(101);
+   s.push(102);
+   s.push(103);
+   s.push(104);
+   s.push(105);
 
 
-	cout << s.size() << endl;
-	cout << s.top() << endl;
-	cout << s.pop() << endl;
-	cout << s.pop() << endl;
-	cout << s.pop() << endl;
-	cout << s.pop() << endl;
-	cout << s.pop() << endl;
-	return 0;
+   cout << s.size() << endl;
+   cout << s.top() << endl;
+   cout << s.pop() << endl;
+   cout << s.pop() << endl;
+   cout << s.pop() << endl;
+   cout << s.pop() << endl;
+   cout << s.pop() << endl;
+   return 0;
 }
