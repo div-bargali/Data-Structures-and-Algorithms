fbb96b8cab0f64cf52903094b7978a13aef1da4b
dishagupta2413 <guptadisha242003@gmail.com>
2021-10-04 01:23:55 +0530

    Knapsack Improvised code

--- C++/Algorithms/Dynamic-Programming/0-1 Knapsack Problem/0-1 Knapsack Problem.cpp
+++ C++/Algorithms/Dynamic-Programming/0-1 Knapsack Problem/0-1 Knapsack Problem.cpp
@@ -1,17 +1,64 @@
- //Only the DP function
-int knapsack(int W, int wt[], int val[], int n)
-{
-    int i,w;
-    int K[n+1][W+1];
-    for(i=0; i=n; i++)
-    for(w=0; w=W; w++)
-    {
-        if(i==0w==0)
-        K[i][w] = 0;
-        else if(wt[i-1]=w)
-        k[i][w] = max(val[i-1]+K[i-1][w-wt[i-1]], K[i-1][w]);
-        else
-        K[i][w] = K[i-1][w];
+ #include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int* wt = new int[n];
+    int* val = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> wt[i];
     }
-    return K[i][w];
+
+    for (int j = 0; j < n; j++) {
+        cin >> val[j];
+    }
+
+    int w;
+    cin >> w;
+
+    cout << knapsack(wt, val, n, w) << "\n";
+
+    delete[] wt;
+    delete[] val;
 }
+
+int knapsack(int* wt, int* val, int n, int W){
+    // matrix to store final result 
+    int mat[2][W+1];
+    memset(mat, 0, sizeof(mat));
+    // iterate through all items
+    int i = 0;
+    while (i < n)// one by one traverse each element
+    {
+        int j = 0;// traverse all wieghts
+        // j <= W;
+        // if i is odd that mean till now we have odd
+        // number of elements so we store result in 1th
+        // indexed row
+        if (i%2!=0) {
+            while (++j <= W)    // check for each value 
+            { 
+                if (wt[i] <= j) // include element 
+                    mat[1][j] = max(val[i] + mat[0][j-wt[i]], mat[0][j] ); 
+                else // exclude element
+                    mat[1][j] = mat[0][j]; 
+            }
+        }
+        // if i is even that mean till now we have even number // of elements so we store result in 0th indexed row
+        else {
+            while(++j <= W) {
+                if (wt[i] <= j) 
+                    mat[0][j] = max(val[i] + mat[1][j-wt[i]], mat[1][j]); 
+                else 
+                    mat[0][j] = mat[1][j]; 
+            }
+        }
+        i++;
+    } // Return mat[0][W] if n is odd, else mat[1][W] 
+    return (n%2 != 0)? mat[0][W] : mat[1][W];
+
+}
+
